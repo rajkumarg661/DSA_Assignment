6f91ead529e3cd1ddfc4ada4649f7de0f8a24fb2
rajkumarg661 <104885801+rajkumarg661@users.noreply.github.com>
2023-06-04 14:33:38 +0530

    Update Solution2.py

--- DSA-Mock Test 1- Python/Solution2.py
+++ DSA-Mock Test 1- Python/Solution2.py
@@ -1,1 +1,22 @@
+class Solution():
+    def FirstUniqchar(self,s):
+        dic={}
+        for i in s:
+            if i not in dic:
+                dic[i]=1
+            else:
+                dic[i]+=1
+        for i in range(len(s)):
+            if(dic[s[i]]==1):
+                return i
+        return -1
+
+obj=Solution()
+print(obj.FirstUniqchar("leetcode"))
+print(obj.FirstUniqchar("loveleetcode"))
+print(obj.FirstUniqchar("aabb"))
+
+
+
+    
 

4891e9e271ae63fb9778106c34fe998a3008a447
rajkumarg661 <104885801+rajkumarg661@users.noreply.github.com>
2023-06-04 14:15:27 +0530

    Update Solution1.py

--- DSA-Mock Test 1- Python/Solution1.py
+++ DSA-Mock Test 1- Python/Solution1.py
@@ -1,1 +1,10 @@
+arr=[0,1,0,3,12]
+curr_index=0
+for i in range(len(arr)):
+    if arr[i]!=0:
+        arr[i],arr[curr_index]=arr[curr_index],arr[i]
+        curr_index+=1
+for i in range(len(arr)):
+    print(arr[i])
 
+    

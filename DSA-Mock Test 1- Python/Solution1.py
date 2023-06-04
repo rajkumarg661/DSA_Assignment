c4426794903805cd48c9c31dcb32b13cd1b04551
rajkumarg661 <104885801+rajkumarg661@users.noreply.github.com>
2023-06-04 14:38:23 +0530

    Update Solution1.py

--- DSA-Mock Test 1- Python/Solution1.py
+++ DSA-Mock Test 1- Python/Solution1.py
@@ -1,10 +1,17 @@
-arr=[0,1,0,3,12]
-curr_index=0
-for i in range(len(arr)):
-    if arr[i]!=0:
-        arr[i],arr[curr_index]=arr[curr_index],arr[i]
-        curr_index+=1
-for i in range(len(arr)):
-    print(arr[i])
+class Solution():
+    def MovesZero(self,arr):
+        curr_idx=0
+        for i in range(len(arr)):
+            if arr[i]!=0:
+                arr[i],arr[curr_idx]=arr[curr_idx],arr[i]
+                curr_idx+=1
+        return arr
+
+
+obj=Solution()
+print(obj.MovesZero([0,1,0,3,12]))
+print(obj.MovesZero([0]))
+
+
 
     

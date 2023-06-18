055589d76d94a52976c575c19a7694efe3312936
rajkumarg661 <104885801+rajkumarg661@users.noreply.github.com>
2023-06-18 15:32:14 +0530

    Update q1.py

--- DSA-Mock Test 2- Python/q1.py
+++ DSA-Mock Test 2- Python/q1.py
@@ -1,1 +1,18 @@
+def find_sqrt(n):
+    if n < 2:
+        return n
+    low, high = 1, n
+    while low <= high:
+        mid = (low + high) // 2
+        if mid * mid == n:
+            return mid
+        elif mid * mid < n:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return high
+ 
+ 
+n=25
+print(find_sqrt(n))
 

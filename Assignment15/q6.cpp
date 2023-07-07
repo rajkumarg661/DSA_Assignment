ad92ddebbe8a9e9f0dc235461b3659e59da80740
rajkumarg661 <104885801+rajkumarg661@users.noreply.github.com>
2023-07-07 15:57:09 +0530

    Update q6.cpp

--- Assignment15/q6.cpp
+++ Assignment15/q6.cpp
@@ -2,18 +2,32 @@
 using namespace std;
 
 int main() {
-	string s="rajkumar";
-	stack<char>st;
-	string ans="";
-	for(int i=0;i<s.size();i++)
-	{
-	    st.push(s[i]);
-	}
-	while(!st.empty())
-	{
-	    ans+=st.top();
-	    st.pop();
-	}
-	cout<<ans;
+	string S="231*+9-";
+	stack<int>st;
+	  for(int i=0;i<S.size();i++)
+        {
+            
+            if(S[i]>= '0' && S[i]<='9')
+                st.push(S[i]-'0');
+            else
+            {
+                int op1=st.top();
+                st.pop();
+                int op2=st.top();
+                st.pop();
+                int ch=S[i];int res;
+                if(ch=='+')
+                    res=op2+op1;
+                else if(ch=='-')
+                    res=op2-op1;
+                else if(ch=='*')
+                    res=op2*op1;
+                else
+                    res=op2/op1;
+                st.push(res);
+            }
+            
+        }
+        cout<<st.top();
 	return 0;
 }
